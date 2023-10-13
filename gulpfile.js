b2ee5ce17e2b54336e9a18413542b2fbb64b2c0d
perpetualgrimace <hello@jamesferrell.me>
2023-10-13 17:45:04 -0400

    configures gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,110 +1,73 @@
-// Gulp config
-var gulp            = require('gulp');
-var sass            = require('gulp-sass');
-var sourcemaps      = require('gulp-sourcemaps');
-var autoprefixer    = require('gulp-autoprefixer');
-var pixrem          = require('gulp-pixrem');
-var cleanCSS        = require('gulp-clean-css');
-var uglify          = require('gulp-uglify');
-var rename          = require('gulp-rename');
-var beepbeep        = require('beepbeep');
-var minifycss       = require('gulp-minify-css');
-var concat          = require('gulp-concat');
-var del             = require('del');
-var browserSync     = require('browser-sync');
-var reload          = browserSync.reload;
+const { src, dest, watch, series, parallel } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const rename = require("gulp-rename");
+const concat = require("gulp-concat");
+const sourcemaps = require("gulp-sourcemaps");
+const browserSync = require("browser-sync").create();
 
-// Define paths for convenience
-var proxyUrl        = 'localhost:8888/bermanadvertising.com/';
-var scssMainPath    = 'assets/scss/*.scss';
-var scssPartialPath = 'assets/scss/**/*.scss';
-var jsPath          = 'assets/js/**/*.js';
-var jsPatternPath   = 'site/patterns/**/*.js';
-var cssBuildPath    = 'assets/build/css';
-var jsBuildPath     = 'assets/build/js';
+// PATHS
+const proxy = "localhost:8888/berman.jamesferrell.me/";
+const paths = {
+  scss: {
+    src: "./assets/scss/*.scss",
+    dest: "./assets/build/css",
+  },
+  js: {
+    src: "./assets/js/**/*.js",
+    dest: "./assets/build/js",
+  },
+};
 
-
-// Error handling
-function handleError(err) {
-  console.log(err);
-  this.emit('end');
-  beepbeep();
+// BROWSER SYNC WITH PHP INSIDE SERVER
+function sync() {
+  browserSync.init({
+    watch: true,
+    proxy,
+  });
 }
 
-
-// Process js
-gulp.task('scripts', function() {
-  return del(jsBuildPath),
-  gulp.src([jsPath, jsPatternPath]).on('error', handleError)
-  // concat files
-  .pipe(concat('main.js'))
-  // rename
-  .pipe(rename({suffix: '.min'}))
-  // minify
-  .pipe(uglify())
-  // set destination
-  .pipe(gulp.dest(jsBuildPath));
-});
-
-
-// Process css for production
-gulp.task('production-styles', function() {
-  return del(cssBuildPath),
-  gulp.src(scssMainPath).on('error', handleError)
-
-    .pipe(sass({outputStyle: 'compressed'})).on('error', handleError)
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions', '> 2%'],
-      cascade: false,
-      remove: false,
-      supports: false,
-      flexbox: 'no-2009',
-      grid: false
-    })).on('error', handleError)
-    // minify css
-    .pipe(cleanCSS()).on('error', handleError)
-    .pipe(rename({suffix: '.production'}))
-    // pixrem
-    .pipe(pixrem('20px')).on('error', handleError)
-    .pipe(gulp.dest(cssBuildPath));
-});
-
-
-// Process css for dev environment
-gulp.task('dev-styles', function() {
-  return del(cssBuildPath),
-  gulp.src(scssMainPath).on('error', handleError)
-
+// TASKS
+function compileSass() {
+  return src(paths.scss.src)
     .pipe(sourcemaps.init())
-      .pipe(sass({outputStyle: 'compressed'})).on('error', handleError)
-      .pipe(rename({suffix: '.dev'}))
+    .pipe(
+      sass({
+        outputStyle: "compressed",
+      })
+    )
+    .pipe(rename({ extname: ".min.css" }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(cssBuildPath))
+    .pipe(dest(paths.scss.dest))
     .pipe(browserSync.stream());
-});
+}
 
+function compileJs() {
+  return src(paths.js.src)
+    .pipe(sourcemaps.init())
+    .pipe(concat("main.min.js"))
+    .pipe(sourcemaps.write())
+    .pipe(dest(paths.js.dest))
+    .pipe(browserSync.stream());
+}
 
-// Browsersync config
-gulp.task('browser-sync', function() {
-  browserSync.init([cssBuildPath + '/*.min.css', jsBuildPath + '/*.min.js'], {
-    proxy: proxyUrl,
-    port: 3000
-  });
-});
+// WATCH
+function watchSass() {
+  watch(paths.scss.src, compileSass);
+}
 
+function watchJs() {
+  watch(paths.js.src, compileJs);
+}
 
-// Reload
-gulp.task('bs-reload', function() {
-  browserSync.reload();
-});
+function watchPhp() {
+  watch(["./**/*.html", "./**/*.php"]).on("change", browserSync.reload);
+}
 
+// DEFAULT TASK
+exports.default = series(
+  compileSass,
+  compileJs,
+  parallel(sync, watchSass, watchJs, watchPhp)
+);
 
-// Watch scss, js and html files
-gulp.task('default', ['dev-styles', 'production-styles', 'scripts', 'browser-sync'], function() {
-  // Watch scss, run the styles task on change
-  gulp.watch([scssMainPath, scssPartialPath], ['dev-styles', 'production-styles']);
-  // Watch js files, run the scripts task on change
-  gulp.watch([jsPath, jsPatternPath], ['scripts']);
-  // Watch php files, run the bs-reload task on change
-  gulp.watch(['site/**/*.php', 'content/**/*.md'], ['bs-reload']).on('error', handleError);
-});
+// exports.default = parallel(sync, watchPhp);
